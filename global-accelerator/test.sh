73a8c5d39d607a60a62e742327bdd47022ac2c6f
Lee Harding <lee.harding@circuitpeople.com>
2024-10-18 14:15:30 -0700

    Echo the region on connect, add missing port 2083. Test All GXs and ports.

--- global-accelerator/test.sh
+++ global-accelerator/test.sh
@@ -1,10 +1,37 @@
 set -e
 
+echo "Testing GX A:"
 GXA_IPS=$(jq -r ".[]|select(.OutputKey==\"GxAIPv4s\")|.OutputValue" global.outputs)
 
 for port in 80 443; do
     for ip in ${GXA_IPS}; do
-        TEST=$((echo "test port ${port} on ${ip}"; sleep 1) | ncat ${ip} 80)
-        echo "${TEST}"
+        echo "Testing port ${port} on ${ip}"
+        echo " - Command: ncat ${ip} ${port}"
+        TEST=$((echo "test for port ${port} on ${ip}"; sleep 1) | ncat ${ip} ${port})
+        echo " - Result: ${TEST}"
+    done
+done
+
+echo "Testing GX B:"
+GXB_IPS=$(jq -r ".[]|select(.OutputKey==\"GxBIPv4s\")|.OutputValue" global.outputs)
+
+for port in 1468; do
+    for ip in ${GXB_IPS}; do
+        echo "Testing port ${port} on ${ip}"
+        echo " - Command: ncat ${ip} ${port}"
+        TEST=$((echo "test for port ${port} on ${ip}"; sleep 1) | ncat ${ip} ${port})
+        echo " - Result: ${TEST}"
+    done
+done
+
+echo "Testing GX C:"
+GXC_IPS=$(jq -r ".[]|select(.OutputKey==\"GxCIPv4s\")|.OutputValue" global.outputs)
+
+for port in 1812 1813 3799 2083; do
+    for ip in ${GXC_IPS}; do
+        echo "Testing port ${port} on ${ip}"
+        echo " - Command: ncat ${ip} ${port}"
+        TEST=$((echo "test for port ${port} on ${ip}"; sleep 1) | ncat ${ip} ${port})
+        echo " - Result: ${TEST}"
     done
 done
