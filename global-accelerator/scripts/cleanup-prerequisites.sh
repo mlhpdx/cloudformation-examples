a14648fbf4b17a0e6c18bbab66848dbbf7c9ef75
Lee Harding <lee.harding@circuitpeople.com>
2024-10-18 09:25:44 -0700

    Clean-up deployment bucket objects.

--- global-accelerator/scripts/cleanup-prerequisites.sh
+++ global-accelerator/scripts/cleanup-prerequisites.sh
@@ -1,5 +1,11 @@
 set -ex
 
+# if deploy-prerequisites.sh created the bucket we need to remove all of the objects before 
+# the stack can be deleted.  If it was an existing bucket, we'll still be nice and clean
+# up any objects we put in it.
+export BUCKET_NAME=$(jq 'map(select(.OutputKey == "BucketName"))[0]|.OutputValue' -r prerequisites.outputs)
+aws s3 rm --recursive s3://${BUCKET_NAME}/${BUCKET_PREFIX}
+
 if (aws cloudformation describe-stacks --stack-name delete-me-gx-prerequisites --no-paginate --region ${AWS_REGION} > /dev/null); then
   aws cloudformation delete-stack --stack-name delete-me-gx-prerequisites --region ${AWS_REGION}
   aws cloudformation wait stack-delete-complete --stack-name delete-me-gx-prerequisites --region ${AWS_REGION}
