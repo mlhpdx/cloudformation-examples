11f4bdd5b1cef9f20fd2c69c7f7da087b05c50d6
Lee Harding <lee.harding@circuitpeople.com>
2024-10-18 11:09:53 -0700

    Use the default prefix if not set, as an added precaution.

--- global-accelerator/scripts/cleanup-prerequisites.sh
+++ global-accelerator/scripts/cleanup-prerequisites.sh
@@ -4,7 +4,7 @@ set -ex
 # the stack can be deleted.  If it was an existing bucket, we'll still be nice and clean
 # up any objects we put in it.
 export BUCKET_NAME=$(jq 'map(select(.OutputKey == "BucketName"))[0]|.OutputValue' -r prerequisites.outputs)
-aws s3 rm --recursive s3://${BUCKET_NAME}/${BUCKET_PREFIX}
+aws s3 rm --recursive s3://${BUCKET_NAME}/${BUCKET_PREFIX:-temp/delete-me-gx} --region ${AWS_REGION} || true
 
 if (aws cloudformation describe-stacks --stack-name delete-me-gx-prerequisites --no-paginate --region ${AWS_REGION} > /dev/null); then
   aws cloudformation delete-stack --stack-name delete-me-gx-prerequisites --region ${AWS_REGION}
